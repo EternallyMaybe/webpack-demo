5350447b19a0e3a8c869ca0dca094b225d75cde4
EternallyMaybe <908624042@qq.com>
2019-09-10 15:06:22 +0800

    增加webpack-react
    Signed-off-by: EternallyMaybe <908624042@qq.com>

---
+++ react/src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/css/style.scss'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello React & Webpack!</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
